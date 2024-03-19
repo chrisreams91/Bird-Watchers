379cb61f65e70de460cf051fc4d99098b6f6dcdf
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-19 15:51:09 -0500

    Updating MyBird entry

--- birdfrontend/src/pages/mybirds.js
+++ birdfrontend/src/pages/mybirds.js
@@ -1,41 +1,10 @@
 import React from "react";
 import { useState } from 'react';
+import EnterMyBirdData from '../components/EnterMyBirdData'
 
 function MyBirds() {
-
-const [formData, setFormData] = useState({
-    name: '',
-    location: '',
-    date: '',
-  });
-
-  const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = (event) => {
-      event.preventDefault();
-      alert(`Name: ${formData.name}, Location: ${formData.location}, Date: ${formData.date}`
-      );
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-    <br />
-      <label htmlFor="name">Bird Name:</label>
-      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-      <br />
-      <br />
-      <label htmlFor="location">Location:</label>
-      <input type="text" id="location" name="location" value={formData.location} onChange={handleChange} />
-      <br />
-      <br />
-      <label htmlFor="date">Date Seen:</label>
-      <input type="date" id="date" name="date" value={formData.date} onChange={handleChange} />
-      <br />
-      <br />
-      <button type="submit">Submit Findings</button>
-    </form>
+   <EnterMyBirdData/>
   );
 }
 
--- birdfrontend/src/pages/search.js
+++ birdfrontend/src/pages/search.js
@@ -1,46 +1,13 @@
 import React from "react";
 import { useState } from "react";
-
-
-const SearchDatabase = () => {
-
-
-	return (
-		<>
-		    <br />
-			Search Community Bird Database pending:
-			<input type="text" placeholder="search for birds here..." className="search" />
-			<br />
-			<h3>Results:</h3>
-			<table>
-                <tbody>
-                    <tr>
-                        <th>Species</th>
-                        <th>Date</th>
-                        <th>Location</th>
-                        <th>Field Notes</th>
-                    </tr>
-                    <tr>
-                        <td>placeholder species</td>
-                        <td>placeholder date</td>
-                        <td>placeholder location</td>
-                        <td>placeholder field notes</td>
-                    </tr>
-                </tbody>
-            </table>
-		</>
-	);
-};
+import SearchDBTableFormat from '../components/SearchDBTableFormat';
 
 export default SearchDatabase;
 
-//don't forget to add tolowercase for case insensitivity
-    //const search = (bird) => { return bird.filter((item)=>item.species.toLowerCase().includes(query) ||
-    //item.date.toLowerCase().includes(query) || item.location.toLowerCase().includes(query) )}
 
-//{bird.map((item) => (
-    //<tr key={item.id}>
-        //<td>{item.species}</td>
-        //...
-    //</tr>
-//))}
+function SearchDatabase() {
+
+	return (
+	    <SearchDBTableFormat/>
+    )
+}
