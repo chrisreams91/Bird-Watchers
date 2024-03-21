99faa8601f37efabb27ce5ea6331e9c6d26b1769
Quincy Nguyen <adambeckar123@gmail.com>
2024-03-21 12:51:56 -0500

    api struggles

--- birdfrontend/src/pages/hotspots.js
+++ birdfrontend/src/pages/hotspots.js
@@ -1,11 +1,22 @@
 import React from "react";
+import ebird from "../components/eBird"
+import { useState, useEffect } from 'react';
+import app from '../App'
 
-const Hotspots = () => {
-	return (
-		<div>
-			<h1>Hotspots</h1>
-		</div>
-	);
-};
+function Hotspots() {
+    fetch('/v2/ref/hotspot/geo?lat={{lat}}&lng={{lng}}', {
+        headers: {
+            'api-key': 'fg0i8qiujsqk'
+        }
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not OK');
+        }
+        return response.json();
+    })
+
+
+}
 
 export default Hotspots;
