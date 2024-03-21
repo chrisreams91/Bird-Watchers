b54e0df34df2d77ca0bc79a42f726a3ba141e015
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-20 23:54:40 -0500

    sound buttons

--- birdfrontend/src/pages/mybirds.js
+++ birdfrontend/src/pages/mybirds.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useState } from 'react';
 import EnterMyBirdData from '../components/EnterMyBirdData'
+import MP3Player from '../components/MP3Player'
+//import CallMyBirdData from '../components/CallMyBirdData'
 
 function MyBirds() {
   return (
+  <div>
    <EnterMyBirdData/>
    //call CallMyBirdData here when component is ready
+   <MP3Player/>
+   </div>
   );
 }
 
