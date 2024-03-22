e73a624ede9ff7806f38942835f25b97686dab4c
LoganT91 <142700393+LoganT91@users.noreply.github.com>
2024-03-21 22:14:36 -0500

    donation button made and donation class closed due to redundancy.

--- birdfrontend/src/pages/home.js
+++ birdfrontend/src/pages/home.js
@@ -7,7 +7,9 @@ const Home = () => {
 
 
 			<h2>Donations</h2>
+			<a href = "https://give.birds.cornell.edu/page/87895/donate/1?ea.tracking.id=PXXXXX01C&utm_source=googlesearch&utm_medium=cpc&utm_campaign=evergreen&gad_source=1&gclid=CjwKCAjw7-SvBhB6EiwAwYdCAZTeUIr9r5-L1xpqmycdNLY59-wW0gQV2ddH-2oHV9a9A45eSpnXWxoCcfcQAvD_BwE">
 			<button type = "button"> Donate to help birds here!</button>
+			</a>
 		</div>
 	);
 };
