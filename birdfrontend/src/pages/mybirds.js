45694f502a1a3c428e13362d6639cc4a96ebf841
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-22 18:42:28 -0500

    data can be called to FE

--- birdfrontend/src/pages/mybirds.js
+++ birdfrontend/src/pages/mybirds.js
@@ -5,15 +5,12 @@ import MP3Player from '../components/MP3Player'
 import MP3Upload from '../components/MP3Upload'
 
 
-//import CallMyBirdData from '../components/CallMyBirdData'
-
 function MyBirds() {
   return (
   <div>
    <EnterMyBirdData/>
-
    <MP3Player/>
-   //call CallMyBirdData here when component is ready
+
    </div>
   );
 }
