d191983d8c67c9eebe59931c392e6b1a0c6f1c6b
AD <142710797+adyson7@users.noreply.github.com>
2024-03-21 19:44:43 -0500

    Stuff

--- BirdTracker/src/main/java/com/birdwatchers/BirdTracker/controller/SignUpController.java
+++ BirdTracker/src/main/java/com/birdwatchers/BirdTracker/controller/SignUpController.java
@@ -1,4 +1,23 @@
 package com.birdwatchers.BirdTracker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/signup")
 public class SignUpController {
+
+    @Autowired
+    private SignUpService signUpService;
+
+    @PostMapping
+    public ResponseEntity<?> signUp(@RequestBody SignUpRequest request) {
+        try {
+            signUpService.signUp(request.getUsername()request)
+        }
+    }
 }
--- BirdTracker/src/main/java/com/birdwatchers/BirdTracker/model/data/UserRepository.java
+++ BirdTracker/src/main/java/com/birdwatchers/BirdTracker/model/data/UserRepository.java
@@ -2,5 +2,6 @@ package com.birdwatchers.BirdTracker.model.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
