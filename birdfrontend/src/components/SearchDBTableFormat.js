914b8c3ee619a8c95fc1d63acdf964f4584c8fba
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-22 21:08:58 -0500

    Table formats

--- birdfrontend/src/components/SearchDBTableFormat.js
+++ birdfrontend/src/components/SearchDBTableFormat.js
@@ -1,44 +1,56 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+
 
 
 function SearchDBTableFormat() {
+
+    const[birds, setBirds]=useState([])
+
+    /*const search = (bird) => { return bird.filter((item)=>item.species.toLowerCase().includes(query) || item.date.toLowerCase().includes(query) || item.location.toLowerCase().includes(query) )} */
+
+        useEffect(() =>{
+            fetch("http://localhost:8080/mybirds/getAll")
+            .then(res=>res.json())
+            .then((result)=>{
+                setBirds(result);
+            }
+        )
+        },[])
+
+
     return (
 		<>
-		    <br />
-			Search Community Bird Database pending:
-			<input type="text" placeholder="search for birds here..." className="search" />
-			<br />
-			<h3>Results:</h3>
-			<table>
-                <tbody>
-                    <tr>
-                        <th>Species</th>
-                        <th>Date</th>
-                        <th>Location</th>
-                        <th>Field Notes</th>
+            <br />
+            Search Community Bird Database pending:
+            <input type="text" placeholder="search for birds here..." className="search" />
+            <br />
+            <h3>Results:</h3>
+                <div>
+                    <th>IDs</th>
+                    <th>Bird Species</th>
+                    <th>Location Seen</th>
+                    <th>Date Seen</th>
+                    <th>Field Notes</th>
+                    <th>Photo</th>
+                    <th>Sound File</th>
+                  {birds.map((bird) => (
+                    <tr key={bird.id}>
+                      <td>Id: {bird.id}</td>
+                      <td>Name: {bird.bird_species}</td>
+                      <td>Location: {bird.location}</td>
+                      <td>Date: {bird.date}</td>
+                      <td>Field Notes: {bird.description}</td>
+                      <td>Photo: {bird.photo}</td>
+
                     </tr>
-                    <tr>
-                        <td>placeholder species</td>
-                        <td>placeholder date</td>
-                        <td>placeholder location</td>
-                        <td>placeholder field notes</td>
-                    </tr>
-                </tbody>
-            </table>
+                  ))}
+                </div>
 		</>
 	);
 };
 
 export default SearchDBTableFormat;
 
-//don't forget to add tolowercase for case insensitivity
-    //const search = (bird) => { return bird.filter((item)=>item.species.toLowerCase().includes(query) ||
-    //item.date.toLowerCase().includes(query) || item.location.toLowerCase().includes(query) )}
 
-//{bird.map((item) => (
-    //<tr key={item.id}>
-        //<td>{item.species}</td>
-        //...
-    //</tr>
-//))}
+
