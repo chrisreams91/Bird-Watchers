8e295e5a2a146c40fb297f0b28584e45b66408f8
LoganT91 <142700393+LoganT91@users.noreply.github.com>
2024-04-21 19:05:30 -0500

    update for blog

--- birdfrontend/src/components/Update.js
+++ birdfrontend/src/components/Update.js
@@ -1,41 +1,69 @@
-import React from 'react';
-import { useState, useEffect, useRef } from 'react';
-import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from 'react-router-dom';
-import { Link, useNavigate, BrowserRouter } from 'react-router-dom';
-import { Routes,Route } from "react-router-dom";
+import styles from "../mybirds.css";
+import axios from "axios";
+import { jwtDecode } from 'jwt-decode';
+
 function Update() {
 
-    const[blogText, setBlogText]=useState('');
     const[title,setTitle]=useState('');
-    const {id} = useParams();
-    const [values, setValues] = useState({
-        id: id,
-        title: '',
-        blogText: ''
-    })
+      const[blogText, setBlogText]=useState('');
+      const[blogs, setBlogs]=useState([]);
+      const titleName = useRef("");
+      const textName = useRef("");
 
-    useEffect(()=> {
-        const token = localStorage.getItem('jwtToken');
-        axios.get(`http://localhost:8080/blogposts/add/${id}`+id, {
-                headers: {
-                  Authorization: `Bearer ${token}`
-                }
-        })
-        .then(res => {
-            setValues({...values, title: res.data.title, blogText: res.data.blogText})
-        })
-        .catch(err => console.log(err))
-    }, [])
 
-    const navigate = useNavigate();
+        const [username, setUsername] = useState('');
+          useEffect(() => {
+            const token = localStorage.getItem('jwtToken');
+            if (token) {
+              const decodedToken = jwtDecode(token);
+              setUsername(decodedToken.sub);
+            }
+          }, []);
 
-    const handleSubmit = () => {
-        axios.put(`http://localhost:8080/blogposts/add/${id}`+id, values)
-        .then(res => {
-            navigate('/');
-        })
-    }
+
+        const [blogData, setBlogData] = useState({
+            title: '',
+            blogText: ''
+          });
+
+           const {id} = useParams();
+
+           const handleInputChange = (e) => {
+               const { name, value } = e.target;
+               setBlogData({
+                 ...blogData,
+                 [name]: value
+               });
+             };
+
+
+         const handleSubmit = async (e) => {
+           e.preventDefault();
+           try {
+             const token = localStorage.getItem('jwtToken');
+             if (!token) {
+               throw new Error('No JWT token found');
+             }
+             const response = await axios.put(
+               `http://localhost:8080/blogposts/add/${id}`,
+               {
+                 ...blogData,
+                 username: username
+               },
+               {
+                 headers: {
+                   Authorization: `Bearer ${token}`
+                 }
+               }
+             );
+             console.log(response);
+           } catch (err) {
+             console.error(err);
+           }
+         };
+
 
     return (
            <div>
@@ -45,13 +73,13 @@ function Update() {
 
                 <br />
                   <label htmlFor="title" className="loginEntry">Title</label>
-                  <input type="text" className="loginEntry" id="title" name="title" value={values.title} onChange={e => setValues({...values, title: e.target.value})} required/>
+                  <input type="text" className="loginEntry" ref={titleName} id="title" name="title" value={blogData.title} onChange={handleInputChange} required/>
 
                   <br />
                   <br />
 
                    <label htmlFor="blogText"  className="loginEntry">Blog</label>
-                   <textarea id="blogText"  className="loginEntry" name="blogText" value={values.blogText} onChange={e => setValues({...values, blogText: e.target.value})} required></textarea>
+                   <textarea id="blogText"  className="loginEntry" ref={textName} name="blogText" value={blogData.blogText} onChange={handleInputChange} required></textarea>
 
                    <br />
                    <br />
